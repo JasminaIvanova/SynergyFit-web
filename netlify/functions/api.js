d58ad7b3f7a092ec388f7dc956b933dd1d5a00e6
JasminaIvanova <jasmina.j.ivanova@gmail.com>
2026-03-04 14:59:46 +0200

    fix routing

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,4 +1,18 @@
 const serverless = require('serverless-http');
 const app = require('../../server/index');
 
-module.exports.handler = serverless(app);
+const handler = serverless(app);
+
+module.exports.handler = async (event, context) => {
+  // Netlify redirect strips /api from the path.
+  // Restore it so Express can match routes defined under /api/*
+  if (!event.path.startsWith('/api')) {
+    event.path = '/api' + event.path;
+  }
+  if (event.rawUrl && !new URL(event.rawUrl).pathname.startsWith('/api')) {
+    const url = new URL(event.rawUrl);
+    url.pathname = '/api' + url.pathname;
+    event.rawUrl = url.toString();
+  }
+  return handler(event, context);
+};
